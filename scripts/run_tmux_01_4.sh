deb2ca1541a8a90a7ab0aede31351b41a4d628e2
Cheol-Will <kangcs7083@naver.com>
2025-09-14 08:00:19 +0000

    temp

--- scripts/run_tmux_01_4.sh
+++ scripts/run_tmux_01_4.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# MemAE-d64-lr0.005-t0.1
+
 data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography "satimage-2" nslkdd fraud  shuttle census) # from MCM
-depth=5
 hidden_dim=64
-learning_rate=0.001
-entropy_loss_weight=0.001
+learning_rate=0.005
 temperature=0.1
-model_type="MemPAE"
+model_type="MemAE"
 
 for data in "${data_list[@]}"; do
-    exp_name="$model_type-use_ent_score-ent$entropy_loss_weight-L$depth-d$hidden_dim-lr$learning_rate-t$temperature"
+    exp_name="$model_type-d$hidden_dim-lr$learning_rate-t$temperature"
     echo "Run $exp_name on $data"
     python main.py \
         --dataname "$data" \
         --model_type $model_type \
-        --use_entropy_loss_as_score \
-        --entropy_loss_weight $entropy_loss_weight\
-        --depth $depth \
         --hidden_dim "$hidden_dim" \
         --learning_rate "$learning_rate" \
         --temperature "$temperature" \
