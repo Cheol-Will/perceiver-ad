2e263d4b99c9a261c85da413dc1b919baca0653a
Cheol-Will <kangcs7083@naver.com>
2025-09-14 13:49:41 +0000

    temp

--- scripts/run_tmux_01_2.sh
+++ scripts/run_tmux_01_2.sh
@@ -5,11 +5,11 @@ data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc 
 depth=4
 hidden_dim=64
 learning_rate=0.001
-temperature=0.2
+temperature=0.1
 model_type="MemPAE"
 
 for data in "${data_list[@]}"; do
-    exp_name="$model_type-ws-pos_query+token-L$depth-d$hidden_dim-lr$learning_rate-t$temperature"
+    exp_name="$model_type-ws-pos_query+token-large_mem_x2-L$depth-d$hidden_dim-lr$learning_rate-t$temperature"
     echo "$exp_name on $data"
     python main.py \
         --dataname "$data" \
@@ -17,6 +17,8 @@ for data in "${data_list[@]}"; do
         --is_weight_sharing \
         --use_pos_enc_as_query \
         --use_mask_token \
+        --num_memories_not_use_power_of_two \
+        --num_memories_not_use_power_of_two \
         --depth $depth \
         --hidden_dim "$hidden_dim" \
         --learning_rate "$learning_rate" \
