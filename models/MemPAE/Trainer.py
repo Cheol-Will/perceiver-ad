6f827a4898a932e017d2525ab7536ddecbcaf113
Cheol-Will <kangcs@naver.com>
2025-09-14 13:53:15 +0000

    temp

--- models/MemPAE/Trainer.py
+++ models/MemPAE/Trainer.py
@@ -23,10 +23,12 @@ class Trainer(object):
         if model_config['num_memories'] is None:
             num_train = self.get_num_train()
             if train_config['num_memories_not_use_power_of_two']:
-                model_config['num_memories'] = int(math.sqrt(num_train))
+                if train_config['num_memories_twice']:
+                    model_config['num_memories'] = int(math.sqrt(num_train)) * 2
+                else:
+                    model_config['num_memories'] = int(math.sqrt(num_train))
             else:
                 model_config['num_memories'] = nearest_power_of_two(int(math.sqrt(num_train)))
-        if train_config['num_memories_twice']:
             model_config['num_memories'] *= 2 # times 2
 
         self.device = train_config['device']
