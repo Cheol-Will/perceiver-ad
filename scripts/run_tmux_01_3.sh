b3a9675138286b584bfc2576a9a792eae9c28049
Cheol-Will <kangcs7083@naver.com>
2025-09-14 01:45:22 +0000

    temp

--- scripts/run_tmux_01_3.sh
+++ scripts/run_tmux_01_3.sh
@@ -5,15 +5,20 @@ data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc 
 depth=4
 hidden_dim=64
 learning_rate=0.001
-model_type="PAE"
+temperature=0.1
+model_type="MemPAE"
+
 for data in "${data_list[@]}"; do
-    echo "Running $model_type data=$data dim=$hidden_dim learning_rate=$learning_rate weight sharing"
-    exp_name="$model_type-L$depth-d$hidden_dim-lr$learning_rate"
+    exp_name="$model_type-pos_query+token-L$depth-d$hidden_dim-lr$learning_rate-t$temperature"
+    echo "$exp_name on $data"
     python main.py \
         --dataname "$data" \
         --model_type $model_type \
+        --use_pos_enc_as_query \
+        --use_mask_token \
         --depth $depth \
         --hidden_dim "$hidden_dim" \
         --learning_rate "$learning_rate" \
+        --temperature "$temperature" \
         --exp_name "$exp_name"
 done
