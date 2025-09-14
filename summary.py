f75d3a746aaf3283d537d17ef14e781246bdd270
Cheol-Will <kangcs@naver.com>
2025-09-14 13:28:22 +0000

    commit

--- summary.py
+++ summary.py
@@ -469,13 +469,14 @@ def main(args):
         # 'MemAE-d256-lr0.01-t0.1', # 0.6893    5.1875 (KNN: 4.0000)
         # 'MemAE-l2-d128-lr0.005', # 0.6744    4.8750 (KNN: 4.1250)
 
+        'PAE-ws-L6-d64-lr0.001', # 1.2 
+        'MemPAE-ws-pos_query+token-large_mem-L4-d64-lr0.001-t0.1', # GOOD!!!!!!!!!!!!!!!!
+
         # 'PAE-L4-d64-lr0.001', # 1.2
-        'PAE-L6-d64-lr0.001', # Need to check 
-        # 'PAE-ws-L6-d64-lr0.001', # 1.2 
+        # 'PAE-L6-d64-lr0.001', # Need to check 
         # 'MemAE-d64-lr0.005-t0.1',
         # 'PAE-ws-d64-lr0.001', # 1.2
         # 'MemPAE-ws-pos_query+token-large_mem-use_ent_score-ent0.001-L4-d64-lr0.001-t0.1',
-        'MemPAE-ws-pos_query+token-large_mem-L4-d64-lr0.001-t0.1', # GOOD!!!!!!!!!!!!!!!!
         # 'MemPAE-pos_query+token-L4-d64-lr0.001-t0.1',
         
         # 'MemPAE-ws-pos_query+token-L4-d64-lr0.001-t0.2',
