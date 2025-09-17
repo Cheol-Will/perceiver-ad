89edcf7fbb20c3c1cb3df4f1ade1fda3bd430bf9
Cheol-Will <kangcs7083@naver.com>
2025-09-17 00:23:37 +0000

    temp

--- scripts/run_tmux_01_1.sh
+++ scripts/run_tmux_01_1.sh
@@ -18,7 +18,8 @@ set -euo pipefail
 #     contamination_0.03_23_mammography_42
 #     contamination_0.05_23_mammography_42
 # )
-data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography "satimage-2" nslkdd fraud  shuttle census) # from MCM
+data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography "satimage-2" nslkdd fraud ) # from MCM
+# data_list=(shuttle census)
 sim_type='cross_attn'
 depth=4
 hidden_dim=64
@@ -27,17 +28,16 @@ temperature=0.1
 model_type="MemPAE"
 
 for data in "${data_list[@]}"; do
-    exp_name="$model_type-ws-rin-pos_query+token-d$hidden_dim-lr$learning_rate-t$temperature"
+    exp_name="$model_type-ws-$sim_type-rin-L$depth-d$hidden_dim-lr$learning_rate"
     echo "$exp_name on $data"
     python main.py \
         --dataname "$data" \
         --model_type $model_type \
         --is_weight_sharing \
+        --sim_type "$sim_type" \
         --is_recurrent \
-        --use_pos_enc_as_query \
-        --use_mask_token \
+        --depth $depth \
         --hidden_dim "$hidden_dim" \
         --learning_rate "$learning_rate" \
-        --temperature "$temperature" \
         --exp_name "$exp_name"
 done
--- scripts/run_tmux_01_6.sh
+++ scripts/run_tmux_01_6.sh
@@ -18,16 +18,17 @@ set -euo pipefail
 #     contamination_0.03_23_mammography_42
 #     contamination_0.05_23_mammography_42
 # )
-data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography "satimage-2" nslkdd fraud  shuttle census) # from MCM
+# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography "satimage-2" nslkdd fraud ) # from MCM
+data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography "satimage-2" nslkdd fraud ) # from MCM
 sim_type='cross_attn'
-depth=6
+depth=3
 hidden_dim=64
 learning_rate=0.001
 temperature=0.1
 model_type="MemPAE"
 
 for data in "${data_list[@]}"; do
-    exp_name="$model_type-ws-$sim_type-rin-pos_query+token-L$depth-d$hidden_dim-lr$learning_rate"
+    exp_name="$model_type-ws-$sim_type-rin-L$depth-d$hidden_dim-lr$learning_rate"
     echo "$exp_name on $data"
     python main.py \
         --dataname "$data" \
@@ -35,8 +36,6 @@ for data in "${data_list[@]}"; do
         --is_weight_sharing \
         --sim_type "$sim_type" \
         --is_recurrent \
-        --use_pos_enc_as_query \
-        --use_mask_token \
         --depth $depth \
         --hidden_dim "$hidden_dim" \
         --learning_rate "$learning_rate" \
