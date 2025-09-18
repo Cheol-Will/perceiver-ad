4ef78d653a06c9db60ab12198712632d33c6bd24
Cheol-Will <kangcs7083@naver.com>
2025-09-18 12:30:30 +0000

    temp

--- scripts/run_tmux_01_1.sh
+++ scripts/run_tmux_01_1.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite) # from MCM
+# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite) # from MCM
+data_list=(campaign mammography "satimage-2")
 hidden_dim=64
 learning_rate=0.001
 temperature=0.1
 model_type="MemPAE"
-memory_ratio=0.5
+latent_ratio=2.0
 
 for data in "${data_list[@]}"; do
-    exp_name="$model_type-ws-pos_query+token-memory_ratio$memory_ratio-d$hidden_dim-lr$learning_rate-t$temperature"
+    exp_name="$model_type-ws-pos_query+token-latent_ratio$latent_ratio-d$hidden_dim-lr$learning_rate-t$temperature"
     echo "Running $exp_name on $data."
     python main.py \
         --dataname "$data" \
@@ -17,7 +18,7 @@ for data in "${data_list[@]}"; do
         --is_weight_sharing \
         --use_pos_enc_as_query \
         --use_mask_token \
-        --memory_ratio "$memory_ratio"\
+        --latent_ratio "$latent_ratio"\
         --hidden_dim "$hidden_dim" \
         --learning_rate "$learning_rate" \
         --temperature "$temperature" \
--- scripts/run_tmux_01_5.sh
+++ scripts/run_tmux_01_5.sh
@@ -1,44 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# data_list=(
-#     contamination_0.01_18_Ionosphere_42
-#     contamination_0.03_18_Ionosphere_42
-#     contamination_0.05_18_Ionosphere_42
-#     contamination_0.01_32_shuttle_42
-#     contamination_0.03_32_shuttle_42
-#     contamination_0.05_32_shuttle_42
-#     contamination_0.01_38_thyroid_42
-#     contamination_0.03_38_thyroid_42
-#     contamination_0.05_38_thyroid_42
-#     contamination_0.01_26_optdigits_42
-#     contamination_0.03_26_optdigits_42
-#     contamination_0.05_26_optdigits_42
-#     contamination_0.01_23_mammography_42
-#     contamination_0.03_23_mammography_42
-#     contamination_0.05_23_mammography_42
-# )
-data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography "satimage-2" nslkdd fraud  shuttle census) # from MCM
-sim_type='cross_attn'
-depth=2
+# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite) # from MCM
+data_list=(campaign mammography "satimage-2")
 hidden_dim=64
 learning_rate=0.001
 temperature=0.1
 model_type="MemPAE"
+memory_ratio=4.0
 
 for data in "${data_list[@]}"; do
-    exp_name="$model_type-ws-$sim_type-rin-pos_query+token-L$depth-d$hidden_dim-lr$learning_rate"
-    echo "$exp_name on $data"
+    exp_name="$model_type-ws-pos_query+token-memory_ratio$memory_ratio-d$hidden_dim-lr$learning_rate-t$temperature"
+    echo "Running $exp_name on $data."
     python main.py \
         --dataname "$data" \
         --model_type $model_type \
         --is_weight_sharing \
-        --sim_type "$sim_type" \
-        --is_recurrent \
         --use_pos_enc_as_query \
         --use_mask_token \
-        --depth $depth \
+        --memory_ratio "$memory_ratio"\
         --hidden_dim "$hidden_dim" \
         --learning_rate "$learning_rate" \
+        --temperature "$temperature" \
         --exp_name "$exp_name"
 done
