7a8fc7c208b94658f90021612e4c0cc6292bc827
Cheol-Will <kangcs@naver.com>
2025-09-18 15:42:57 +0000

    temp

--- scripts/run_mcmpae.sh
+++ scripts/run_mcmpae.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere hepatitis pima wbc wine thyroid optdigits pendigits satellite campaign mammography) # from MCM
+# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere hepatitis pima wbc wine thyroid optdigits pendigits satellite campaign mammography) # from MCM
+
+data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography "satimage-2" nslkdd fraud  shuttle census) # from MCM
 
 hidden_dim=64
-learning_rate=0.005
+learning_rate=0.001
 temperature=0.1
 model_type="MCMPAE"
 for data in "${data_list[@]}"; do
