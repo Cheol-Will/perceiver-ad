2ee1d2c834526e6283fee0f5fade5b214b084f90
Cheol-Will <kangcs7083@naver.com>
2025-09-19 00:19:49 +0000

    temp

--- scripts/run_tmux_01_0.sh
+++ scripts/run_tmux_01_0.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite) # from MCM
-data_list=(campaign mammography "satimage-2")
-hidden_dim=64
-learning_rate=0.001
-temperature=0.1
-model_type="MemPAE"
-latent_ratio=0.5
+# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere hepatitis pima wbc wine thyroid optdigits pendigits satellite campaign mammography) # from MCM
 
+data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography "satimage-2" nslkdd fraud  shuttle census) # from MCM
+
+hidden_dim=64
+learning_rate=0.005
+temperature=0.1
+model_type="MCMPAE"
 for data in "${data_list[@]}"; do
-    exp_name="$model_type-ws-pos_query+token-latent_ratio$latent_ratio-d$hidden_dim-lr$learning_rate-t$temperature"
-    echo "Running $exp_name on $data."
+    exp_name="$model_type-ws-pos_query+token-d$hidden_dim-lr$learning_rate"
+    echo "$exp_name on $data"
     python main.py \
         --dataname "$data" \
         --model_type $model_type \
         --is_weight_sharing \
         --use_pos_enc_as_query \
         --use_mask_token \
-        --latent_ratio "$latent_ratio"\
         --hidden_dim "$hidden_dim" \
         --learning_rate "$learning_rate" \
-        --temperature "$temperature" \
         --exp_name "$exp_name"
 done
