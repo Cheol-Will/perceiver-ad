d7cdcd648b91d948606b841634ed83623b215d0f
Cheol-Will <kangcs7083@naver.com>
2025-09-19 17:13:45 +0000

    temp

--- scripts/run_tmux_01_2.sh
+++ scripts/run_tmux_01_2.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere hepatitis pima wbc wine thyroid optdigits pendigits satellite campaign mammography) # from MCM
-
-data_list=(fraud  shuttle census) # from MCM
-
+data_list=(nslkdd shuttle fraud census)   
 hidden_dim=64
-learning_rate=0.005
+learning_rate=0.001
 temperature=0.1
-model_type="MCMPAE"
+model_type="MemPAE"
+memory_ratio=4.0
+
 for data in "${data_list[@]}"; do
-    exp_name="$model_type-ws-pos_query+token-d$hidden_dim-lr$learning_rate"
-    echo "$exp_name on $data"
+    exp_name="$model_type-ws-pos_query+token-memory_ratio$memory_ratio-d$hidden_dim-lr$learning_rate-t$temperature"
+    echo "Running $exp_name on $data."
     python main.py \
         --dataname "$data" \
         --model_type $model_type \
         --is_weight_sharing \
         --use_pos_enc_as_query \
         --use_mask_token \
+        --memory_ratio "$memory_ratio"\
         --hidden_dim "$hidden_dim" \
         --learning_rate "$learning_rate" \
+        --temperature "$temperature" \
         --exp_name "$exp_name"
 done
--- scripts/run_tmux_01_5.sh
+++ scripts/run_tmux_01_5.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite) # from MCM
-data_list=(campaign mammography "satimage-2")
+data_list=(nslkdd shuttle fraud census)   
 hidden_dim=64
 learning_rate=0.001
 temperature=0.1
 model_type="MemPAE"
-memory_ratio=4.0
+latent_ratio=4.0
 
 for data in "${data_list[@]}"; do
-    exp_name="$model_type-ws-pos_query+token-memory_ratio$memory_ratio-d$hidden_dim-lr$learning_rate-t$temperature"
+    exp_name="$model_type-ws-pos_query+token-latent_ratio$latent_ratio-d$hidden_dim-lr$learning_rate-t$temperature"
     echo "Running $exp_name on $data."
     python main.py \
         --dataname "$data" \
@@ -18,7 +17,7 @@ for data in "${data_list[@]}"; do
         --is_weight_sharing \
         --use_pos_enc_as_query \
         --use_mask_token \
-        --memory_ratio "$memory_ratio"\
+        --latent_ratio "$latent_ratio"\
         --hidden_dim "$hidden_dim" \
         --learning_rate "$learning_rate" \
         --temperature "$temperature" \
