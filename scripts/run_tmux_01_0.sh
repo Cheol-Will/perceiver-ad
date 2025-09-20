54f8e27e51a2226d21438940de5fcc4ac1f26498
Cheol-Will <kangcs7083@naver.com>
2025-09-20 03:16:08 +0000

    temp

--- scripts/run_tmux_01_0.sh
+++ scripts/run_tmux_01_0.sh
@@ -1,25 +1,22 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
 
-data_list=(nslkdd shuttle fraud census)   
-hidden_dim=64
-learning_rate=0.001
-temperature=0.1
-model_type="MemPAE"
-memory_ratio=0.5
+# Dataset from MCM and Disent-AD
+# model_list=(IForest LOF OCSVM ECOD KNN PCA AutoEncoder DeepSVDD GOAD NeuTraL ICL MCM DRL Disent) 
+# data_list=(arrhythmia optdigits breastw cardio campaign cardiotocography census fraud glass ionosphere mammography nslkdd  pendigits pima satellite "satimage-2" shuttle thyroid wbc wine) 
+# data_list=(census fraud "satimage-2" shuttle) 
+
+data_list=(pima pendigits "satimage-2") 
+model_list=(KNN Disent MCM DRL) 
+train_ratio_list=(0.8)
 
 for data in "${data_list[@]}"; do
-    exp_name="$model_type-ws-pos_query+token-memory_ratio$memory_ratio-d$hidden_dim-lr$learning_rate-t$temperature"
-    echo "Running $exp_name on $data."
-    python main.py \
-        --dataname "$data" \
-        --model_type $model_type \
-        --is_weight_sharing \
-        --use_pos_enc_as_query \
-        --use_mask_token \
-        --memory_ratio "$memory_ratio"\
-        --hidden_dim "$hidden_dim" \
-        --learning_rate "$learning_rate" \
-        --temperature "$temperature" \
-        --exp_name "$exp_name"
+    for model_type in "${model_list[@]}"; do
+        for train_ratio in "${train_ratio_list[@]}"; do
+            python main.py \
+                --dataname "$data" \
+                --model_type "$model_type" \
+                --exp_name "$model_type" \
+                --train_ratio "$train_ratio"
+        done
+    done
 done
--- scripts/run_tmux_01_2.sh
+++ scripts/run_tmux_01_2.sh
@@ -1,25 +1,33 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
 
-data_list=(nslkdd shuttle fraud census)   
+# Dataset from MCM and Disent-AD
+# model_list=(IForest LOF OCSVM ECOD KNN PCA AutoEncoder DeepSVDD GOAD NeuTraL ICL MCM DRL Disent) 
+# data_list=(arrhythmia optdigits breastw cardio campaign cardiotocography census fraud glass ionosphere mammography nslkdd  pendigits pima satellite "satimage-2" shuttle thyroid wbc wine) 
+# data_list=(census fraud "satimage-2" shuttle) 
+
+data_list=(pima pendigits "satimage-2") 
+train_ratio_list=(0.8)
 hidden_dim=64
 learning_rate=0.001
 temperature=0.1
 model_type="MemPAE"
-memory_ratio=4.0
+latent_ratio=4.0
+train_ratio_list=(0.8 0.5)
 
 for data in "${data_list[@]}"; do
-    exp_name="$model_type-ws-pos_query+token-memory_ratio$memory_ratio-d$hidden_dim-lr$learning_rate-t$temperature"
-    echo "Running $exp_name on $data."
-    python main.py \
-        --dataname "$data" \
-        --model_type $model_type \
-        --is_weight_sharing \
-        --use_pos_enc_as_query \
-        --use_mask_token \
-        --memory_ratio "$memory_ratio"\
-        --hidden_dim "$hidden_dim" \
-        --learning_rate "$learning_rate" \
-        --temperature "$temperature" \
-        --exp_name "$exp_name"
+    for train_ratio in "${train_ratio_list[@]}"; do
+        exp_name="$model_type-ws-pos_query+token-d$hidden_dim-lr$learning_rate-t$temperature"
+        echo "Running $exp_name on $data."
+        python main.py \
+            --dataname "$data" \
+            --model_type $model_type \
+            --is_weight_sharing \
+            --use_pos_enc_as_query \
+            --use_mask_token \
+            --hidden_dim "$hidden_dim" \
+            --learning_rate "$learning_rate" \
+            --temperature "$temperature" \
+            --exp_name "$exp_name" \
+            --train_ratio "$train_ratio"
+    done
 done
