ecd68ff56bd09b7e60d18d7d30ffca32bd3a9a81
Cheol-Will <kangcs7083@naver.com>
2025-09-20 04:38:12 +0000

    temp

--- scripts/run_tmux_01_1.sh
+++ scripts/run_tmux_01_1.sh
@@ -5,28 +5,20 @@
 # data_list=(arrhythmia optdigits breastw cardio campaign cardiotocography census fraud glass ionosphere mammography nslkdd  pendigits pima satellite "satimage-2" shuttle thyroid wbc wine) 
 # data_list=(census fraud "satimage-2" shuttle) 
 
-data_list=(arrhythmia cardio cardiotocography mammography) 
+# data_list=(pendigits "satimage-2" breastw pima glass ionosphere wbc wine thyroid arrhythmia cardio cardiotocography mammography) 
+data_list=(arrhythmia) 
+
+model_list=(KNN Disent MCM DRL) 
 train_ratio_list=(0.3)
-hidden_dim=64
-learning_rate=0.001
-temperature=0.1
-model_type="MemPAE"
-latent_ratio=4.0
 
 for data in "${data_list[@]}"; do
-    for train_ratio in "${train_ratio_list[@]}"; do
-        exp_name="$model_type-ws-pos_query+token-d$hidden_dim-lr$learning_rate-t$temperature"
-        echo "Running $exp_name on $data."
-        python main.py \
-            --dataname "$data" \
-            --model_type $model_type \
-            --is_weight_sharing \
-            --use_pos_enc_as_query \
-            --use_mask_token \
-            --hidden_dim "$hidden_dim" \
-            --learning_rate "$learning_rate" \
-            --temperature "$temperature" \
-            --exp_name "$exp_name" \
-            --train_ratio "$train_ratio"
+    for model_type in "${model_list[@]}"; do
+        for train_ratio in "${train_ratio_list[@]}"; do
+            python main.py \
+                --dataname "$data" \
+                --model_type "$model_type" \
+                --exp_name "$model_type" \
+                --train_ratio "$train_ratio"
+        done
     done
 done
