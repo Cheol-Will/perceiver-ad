d543739f55f66bea486964bee5ba5ee03c2ac877
Cheol-Will <kangcs@naver.com>
2025-09-22 13:45:55 +0000

    temp

--- scripts/run_analysis.sh
+++ scripts/run_analysis.sh
@@ -1,61 +1,22 @@
 #!/bin/bash
 
-# data_list=(satimage-2) # from MCM
 data_list=(pendigits optdigits arrhythmia breastw glass ionosphere pima wbc wine cardio cardiotocography thyroid) # from MCM
-
-# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography ) # from MCM
-# data_list=("satimage-2") # from MCM
-# data_list=(pendigits) # from MCM
-
-# data_list=(cardio cardiotocography) # from MCM
-
-# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign) # from MCM
-# data_list=(mammography census fraud shuttle nslkdd) # from MCM
-# data_list=(pendigits) # from MCM
-# data_list=(campaign) # from MCM
-# data_list=(shuttle) # from MCM
 data_list=(pendigits optdigits arrhythmia breastw glass ionosphere pima wbc wine cardio cardiotocography thyroid) # from MCM
-# data_list=(breastw cardio ionosphere pendigits "satimage-2" shuttle wbc)
-# data_list=(pendigits) # from MCM
-# data_list=(cardiotocography) # from MCM
 data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign) # from MCM
-
-# data_list=(pima) # from MCM
-# data_list=(wine wbc) # from MCM
-# data_list=(cardiotocography) # from MCM
-# data_list=(
-#     # dependency_anomalies_45_wine_42
-#     # dependency_anomalies_14_glass_42
-#     # dependency_anomalies_42_WBC_42
-#     # dependency_anomalies_18_Ionosphere_42
-#     # dependency_anomalies_4_breastw_42
-#     # dependency_anomalies_29_Pima_42
-#     # dependency_anomalies_6_cardio_42
-#     # dependency_anomalies_7_Cardiotocography_42
-#     # dependency_anomalies_38_thyroid_42
-#     # dependency_anomalies_26_optdigits_42
-#     # dependency_anomalies_31_satimage-2_42
-#     # dependency_anomalies_30_satellite_42
-#     # dependency_anomalies_23_mammography_42
-#     # dependency_anomalies_5_campaign_42
-#     # dependency_anomalies_32_shuttle_42
-#     mnist
-# )
-
-
 
 hidden_dim=64
 learning_rate=0.001
 temperature=0.1
 model_type="MemPAE"
-# latent_ratio=4.0
 train_ratio_list=(1.0)
 lambda=0.01
-top_k=5
+top_k=1
 depth=5
+sim_type='l2'
 for data in "${data_list[@]}"; do
     for train_ratio in "${train_ratio_list[@]}"; do
-        exp_name="$model_type-ws-pos_query+token-np-lambda-top$top_k-L$depth-d$hidden_dim-lr$learning_rate-t$temperature"
+        exp_name="$model_type-ws-pos_query+token-np-$sim_type-top$top_k-L$depth-d$hidden_dim-lr$learning_rate-t$temperature"
+        # exp_name="$model_type-ws-pos_query+token-np-lambda-top$top_k-L$depth-d$hidden_dim-lr$learning_rate-t$temperature"
         echo "Running $exp_name on $data."
         python analysis.py \
             --dataname "$data" \
@@ -64,6 +25,7 @@ for data in "${data_list[@]}"; do
             --use_pos_enc_as_query \
             --use_mask_token \
             --not_use_power_of_two\
+            --sim_type "$sim_type"\
             --latent_loss_weight "$lambda"\
             --top_k $top_k\
             --depth $depth\
