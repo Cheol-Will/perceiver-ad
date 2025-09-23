e366f760072765c9ed21b528cb27fcd29f851175
Cheol-Will <kangcs7083@naver.com>
2025-09-23 19:50:21 +0000

    temp:

--- scripts/run_tmux_01_1.sh
+++ scripts/run_tmux_01_1.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography) # from MCM
-data_list=(
-    thyroid
-    optdigits
-    "satimage-2"
-    satellite
-    # campaign
-    # nslkdd
-    # fraud
-    # census
-) 
-model_list=(DRL MCM Disent) 
-
-train_ratio_list=(0.2 0.4 0.6 0.8)
+data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite mammography "satimage-2" campaign census fraud nslkdd shuttle census fraud nslkdd shuttle) # from MCM
+# data_list=(census fraud )
+# data_list=(nslkdd shuttle)
+hidden_dim=64
+learning_rate=0.001
+temperature=0.1
+model_type="MemPAE"
+train_ratio_list=(1.0)
 for data in "${data_list[@]}"; do
     for train_ratio in "${train_ratio_list[@]}"; do
-        for model_type in "${model_list[@]}"; do
-            python main.py \
-                --dataname "$data" \
-                --model_type $model_type \
-                --train_ratio "$train_ratio"
-        done
+        exp_name="$model_type-ws-pos_query+token-mlp_dec-d$hidden_dim-lr$learning_rate-t$temperature"
+        echo "Running $exp_name on $data."
+        python main.py \
+            --dataname "$data" \
+            --model_type $model_type \
+            --is_weight_sharing \
+            --use_pos_enc_as_query \
+            --use_mask_token \
+            --mlp_decoder \
+            --hidden_dim "$hidden_dim" \
+            --learning_rate "$learning_rate" \
+            --exp_name "$exp_name" \
+            --train_ratio "$train_ratio"
     done
 done
