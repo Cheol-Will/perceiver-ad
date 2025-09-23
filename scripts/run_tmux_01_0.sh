7ceb5d9b7d28ce62627d7751be5a0d6b6f8a0dfe
Cheol-Will <kangcs7083@naver.com>
2025-09-23 09:03:21 +0000

    temP

--- scripts/run_tmux_01_0.sh
+++ scripts/run_tmux_01_0.sh
@@ -1,45 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography shuttle "satimage-2" nslkdd fraud census) # from MCM
-# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography ) # from MCM
-# data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography ) # from MCM
-# data_list=("satimage-2" nslkdd fraud)
-# data_list=(census)
-
-
-# data_list=(
-#     global_anomalies_45_wine_42
-#     global_anomalies_14_glass_42
-#     global_anomalies_42_WBC_42
-#     global_anomalies_18_Ionosphere_42
-#     global_anomalies_4_breastw_42
-#     global_anomalies_29_Pima_42
-#     global_anomalies_6_cardio_42
-#     global_anomalies_7_Cardiotocography_42
-#     global_anomalies_38_thyroid_42
-#     global_anomalies_26_optdigits_42
-#     global_anomalies_31_satimage-2_42
-#     global_anomalies_30_satellite_42
-#     global_anomalies_23_mammography_42
-#     global_anomalies_5_campaign_42
-#     global_anomalies_32_shuttle_42
-# )      
-   
-# data_list=(arrhythmia  cardio cardiotocography pima  optdigits pendigits satellite) # from MCM
-# data_list=(breastw glass ionosphere  wbc wine thyroid ) 
-
+data_list=(arrhythmia breastw cardio cardiotocography glass ionosphere pima wbc wine thyroid optdigits pendigits satellite campaign mammography) # from MCM
 hidden_dim=64
 learning_rate=0.001
 temperature=0.1
 model_type="MemPAE"
 train_ratio_list=(1.0)
-top_k=5
-depth=5
-sim_type='l2'
+depth=0
 for data in "${data_list[@]}"; do
     for train_ratio in "${train_ratio_list[@]}"; do
-        exp_name="$model_type-ws-pos_query+token-np-$sim_type-top$top_k-L$depth-d$hidden_dim-lr$learning_rate-t$temperature"
+        exp_name="$model_type-ws-pos_query+token-L$depth-d$hidden_dim-lr$learning_rate-t$temperature"
         echo "Running $exp_name on $data."
         python main.py \
             --dataname "$data" \
@@ -47,9 +18,6 @@ for data in "${data_list[@]}"; do
             --is_weight_sharing \
             --use_pos_enc_as_query \
             --use_mask_token \
-            --not_use_power_of_two\
-            --sim_type "$sim_type"\
-            --top_k "$top_k"\
             --depth "$depth"\
             --hidden_dim "$hidden_dim" \
             --learning_rate "$learning_rate" \
